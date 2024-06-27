1385d869004e903985c2c09c40b65ff74587b5da
fakuzatsu <abyerswilson@gmail.com>
2024-06-27 09:02:01 +0100

    revert faster soft reset branch to fix test runner

--- src/main.c
+++ src/main.c
@@ -1,12 +1,10 @@
 #include "global.h"
 #include "crt0.h"
-#include "main_menu.h"
 #include "malloc.h"
 #include "link.h"
 #include "link_rfu.h"
 #include "librfu.h"
 #include "m4a.h"
-#include "new_game.h"
 #include "bg.h"
 #include "rtc.h"
 #include "scanline_effect.h"
@@ -18,7 +16,6 @@
 #include "load_save.h"
 #include "gpu_regs.h"
 #include "agb_flash.h"
-#include "save.h"
 #include "sound.h"
 #include "battle.h"
 #include "battle_controllers.h"
@@ -76,7 +73,6 @@ s8 gPcmDmaCounter;
 void *gAgbMainLoop_sp;
 
 static EWRAM_DATA u16 sTrainerId = 0;
-EWRAM_DATA u8 gSoftResetFlag;
 
 //EWRAM_DATA void (**gFlashTimerIntrFunc)(void) = NULL;
 
@@ -90,7 +86,6 @@ static void ReadKeys(void);
 void InitIntrHandlers(void);
 static void WaitForVBlank(void);
 void EnableVCountIntrAtLine150(void);
-static void CB2_PostSoftResetInit(void);
 
 extern void CB2_FlashNotDetectedScreen(void);
 
@@ -190,11 +185,7 @@ static void InitMainCallbacks(void)
     gTrainerHillVBlankCounter = NULL;
     gMain.vblankCounter2 = 0;
     gMain.callback1 = NULL;
-    if(gSoftResetFlag){
-        SetMainCallback2(CB2_PostSoftResetInit);
-    }else{
-        SetMainCallback2(CB2_InitCopyrightScreenAfterBootup);
-    }
+    SetMainCallback2(gInitialMainCB2);
     gSaveBlock2Ptr = &gSaveblock2.block;
     gPokemonStoragePtr = &gPokemonStorage.block;
 }
@@ -498,15 +489,4 @@ void DoSoftReset(void)
 void ClearPokemonCrySongs(void)
 {
     CpuFill16(0, gPokemonCrySongs, MAX_POKEMON_CRIES * sizeof(struct PokemonCrySong));
-}
-
-static void CB2_PostSoftResetInit(void){
-    SetSaveBlocksPointers(GetSaveBlocksPointersBaseOffset());
-    ResetMenuAndMonGlobals();
-    Save_ResetSaveCounters();
-    LoadGameSave(SAVE_NORMAL);
-    if (gSaveFileStatus == SAVE_STATUS_EMPTY || gSaveFileStatus == SAVE_STATUS_CORRUPT)
-        Sav2_ClearSetDefault();
-    SetPokemonCryStereo(gSaveBlock2Ptr->optionsSound);
-    SetMainCallback2(CB2_InitMainMenu);
 }
--- src/main_menu.c
+++ src/main_menu.c
@@ -173,8 +173,6 @@ static EWRAM_DATA bool8 sStartedPokeBallTask = 0;
 static EWRAM_DATA u16 sCurrItemAndOptionMenuCheck = 0;
 
 static u8 sBirchSpeechMainTaskId;
-
-extern u8 gSoftResetFlag;
 // Static ROM declarations
 
 static u32 InitMainMenu(bool8);
@@ -715,7 +713,7 @@ static void Task_MainMenuCheckBattery(u8 taskId)
         SetGpuReg(REG_OFFSET_BLDALPHA, 0);
         SetGpuReg(REG_OFFSET_BLDY, 7);
 
-        if (gSoftResetFlag || !(RtcGetErrorStatus() & RTC_ERR_FLAG_MASK))
+        if (!(RtcGetErrorStatus() & RTC_ERR_FLAG_MASK))
         {
             gTasks[taskId].func = Task_DisplayMainMenu;
         }
