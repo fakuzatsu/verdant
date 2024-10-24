9ca9981792c392f507056f56e8d23863083c673c
fakuzatsu <abyerswilson@gmail.com>
2024-10-24 20:23:29 +0100

    fix rare candy importance being swapped

--- src/party_menu.c
+++ src/party_menu.c
@@ -5993,7 +5993,7 @@ void ItemUseCB_RareCandy(u8 taskId, TaskFunc task)
 
         if (targetSpecies != SPECIES_NONE)
         {
-            if(ItemId_GetImportance(*itemPtr))
+            if(!ItemId_GetImportance(*itemPtr))
                 RemoveBagItem(gSpecialVar_ItemId, 1);
             FreePartyPointers();
             gCB2_AfterEvolution = gPartyMenu.exitCallback;
@@ -6013,7 +6013,7 @@ void ItemUseCB_RareCandy(u8 taskId, TaskFunc task)
         sFinalLevel = GetMonData(mon, MON_DATA_LEVEL, NULL);
         gPartyMenuUseExitCallback = TRUE;
         UpdateMonDisplayInfoAfterRareCandy(gPartyMenu.slotId, mon);
-        if(ItemId_GetImportance(*itemPtr))
+        if(!ItemId_GetImportance(*itemPtr))
             RemoveBagItem(gSpecialVar_ItemId, 1);
         GetMonNickname(mon, gStringVar1);
         if (sFinalLevel > sInitialLevel)
