a9ffd3e97c625684210e5d6b634a36a590e5957e
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2024-12-03 17:15:29 +0100

    Fixes items preventing other switch in effects (#5732)
    Co-authored-by: Pawkkie <61265402+Pawkkie@users.noreply.github.com>

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -10160,6 +10160,10 @@ BattleScript_BerserkGeneRet_End:
 	end3
 
 BattleScript_BoosterEnergyEnd2::
+	call BattleScript_BoosterEnergyRet
+	end2
+
+BattleScript_BoosterEnergyRet::
 	playanimation BS_SCRIPTING, B_ANIM_HELD_ITEM_EFFECT, sB_ANIM_ARG1
 	call BattleScript_AbilityPopUpScripting
 	printstring STRINGID_BOOSTERENERGYACTIVATES
@@ -10167,7 +10171,7 @@ BattleScript_BoosterEnergyEnd2::
 	printstring STRINGID_STATWASHEIGHTENED
 	waitmessage B_WAIT_TIME_MED
 	removeitem BS_SCRIPTING
-	end2
+	return
 
 BattleScript_EffectSnow::
 	attackcanceler
