0fe1ea5a0d2d52429d65c169be9e2e0a9288aa44
fakuzatsu <abyerswilson@gmail.com>
2025-03-05 15:34:25 +0000

    remove unused variable

--- src/trade.c
+++ src/trade.c
@@ -1049,8 +1049,6 @@ static void Trade_Memcpy(void *dest, const void *src, u32 size)
 static bool8 BufferTradeParties(void)
 {
     u8 id = GetMultiplayerId();
-    int i;
-    struct Pokemon *mon;
 
     switch (sTradeMenu->bufferPartyState)
     {
