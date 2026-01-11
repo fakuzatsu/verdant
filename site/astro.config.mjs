1c4e52e88dcad732bea795f9d61d18f0789db336
fakuzatsu <abyerswilson@gmail.com>
2026-01-11 15:47:27 +0000

    see if this website works or nah

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+site: 'https://fakuzatsu.github.io/verdant/',
 output: 'static',
 vite: {
     plugins: [tailwindcss()],
